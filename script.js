08cb8a87f92dae96e2e9e8c8e9a073e3339e1731
Aadhav Prabu <aadhav.prabu@gmail.com>
2023-08-05 13:49:55 -0700

    env

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-const apiKey = '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';
+require('dotenv').config();
+const apiKey = process.env.API_KEY;
 
 document.addEventListener('DOMContentLoaded', () => {
     const summarizeBtn = document.getElementById('summarizeBtn');
